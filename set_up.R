19e9e13edfe56223c05e8751a1796494626679dc
Derek Amuna <dadadee02@outlook.com>
2023-11-21 22:16:56 +0000

    break up of app to server and ui

--- set_up.R
+++
@@ -1,4 +0,0 @@
-
-library('rsconnect')
-rsconnect::setAccountInfo(name='deena95', token='0BCAEBB1DC9585573F9B8C14D8734C94', secret='3ZjEzuAjYtjc4uw+dtuuWuV4/Le9SiqUDHvg2VcR')
-rsconnect::deployApp('https://github.com/DeeNA95/TYCL')
