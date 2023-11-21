151ae7f3cdb823418ff394ce7a294a738ac78d2f
Derek Amuna <dadadee02@outlook.com>
2023-11-21 23:47:56 +0000

    test sucessful

--- server.R
+++ server.R
@@ -1,3 +1,25 @@
+library(shiny)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(shinyjs)
+library(shinythemes)
+products = read_csv('products.csv') 
+product_groups = unique(products$ProductGroup)
+products_headers = names(products)
+products_headers[3] = "Code"
+names(products) = products_headers
+test = readxl::read_xlsx('test/Oct 23.xlsx')
+test_clean1 = test %>% select(c(Code,Product,Quantity,Total))
+test_clean2 = test_clean1 %>% filter(Total != "NA", Total != "Total")
+test_clean3 = test_clean2 %>% left_join(products[,2:3], by = "Code")
+oct23 = test_clean3 %>% filter(Code != 'NA')
+oct23$Total = as.numeric(oct23$Total)
+oct23$Quantity = as.numeric(oct23$Quantity)
+oct23 = oct23 %>% arrange(desc(Total))
+
+
+
 server <- function(input, output, session) {
   
   tout = reactive({
