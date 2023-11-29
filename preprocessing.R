8df916df2c36fc734e88640053508ead6a1f0f4b
Derek Amuna <dadadee02@outlook.com>
2023-11-29 00:53:38 +0000

    what works 2

--- preprocessing.R
+++ preprocessing.R
@@ -129,12 +129,7 @@
   
   
   
-  user_base <- tibble::tibble(
-    user = c("user1", "user2"),
-    password = c("pass1", "pass2"),
-    permissions = c("admin", "standard"),
-    name = c("User One", "User Two")
-  )
+ 
 }
 
 
--- server.R
+++ server.R
@@ -20,21 +20,6 @@ source('header.R')
 
 server <- function(input, output, session) {
  
-  # call login module supplying data frame, 
-  # user and password cols and reactive trigger
-  credentials <- shinyauthr::loginServer(
-    id = "login",
-    data = user_base,
-    user_col = user,
-    pwd_col = password,
-    log_out = reactive(logout_init())
-  )
-  
-  # call the logout module with reactive trigger to hide/show
-  logout_init <- shinyauthr::logoutServer(
-    id = "logout",
-    active = reactive(credentials()$user_auth)
-  )
   
   
 
