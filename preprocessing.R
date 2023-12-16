70ffe62ad70ab048a6d889ad6a3a7622ef92e9b2
Derek Amuna <dadadee02@outlook.com>
2023-12-16 21:58:44 +0000

    automated file running

--- preprocessing.R
+++ preprocessing.R
@@ -33,69 +33,47 @@
 ##monthlys over the years
 {
   #load and clean up function
-  {
-    load_month = function(month, year) {
-      # month should be in quotes and year should be 2 digits
-      selection = c('Code', 'Product', 'Quantity', 'Total')
+ load_month = function(month, year) {
+  # month should be in quotes and year should be 2 digits
+  selection = c('Code', 'Product', 'Quantity', 'Total')
 
-      read_xlsx(paste0('test/', month, ' ', year, '.xlsx'), trim_ws = T) %>%
-        select(all_of(selection)) %>%
-        subset(!(Code %in% c('NA', 'Code')),Total != 'NA')  %>%
-        filter(Total != 'NA', Quantity != 'Quantity') %>%
+  read_xlsx(paste0('test/', month, ' ', year, '.xlsx'), trim_ws = T) %>%
+    select(all_of(selection)) %>%
+    subset(!(Code %in% c('NA', 'Code')), Total != 'NA') %>%
+    filter(Total != 'NA', Quantity != 'Quantity') %>%
 
-        mutate(
-          Code = as.character(Code),
-          Total = as.numeric(Total),
-          Quantity = as.numeric(Quantity),
-          Product = as.character(Product)
-        )
+    mutate(
+      Code = as.character(Code),
+      Total = as.numeric(Total),
+      Quantity = as.numeric(Quantity),
+      Product = as.character(Product)
+    )
+}
+
+# Loading months
+yars = c(22, 23)
+all_data_frames = list()
+
+for (y in yars) {
+  for (i in month.abb) {
+    file_path <- paste0('test/', i, ' ', y, '.xlsx')
+    
+    if (file.exists(file_path)) {
+      processed_name <- paste0(i, y)
+      data_frame <- load_month(i, y) %>% mutate(month = i, year = y)
+      
+      all_data_frames[[processed_name]] <- data_frame
     }
   }
+}
 
-  #loading months
+# Combining data frames
+tot_test <- bind_rows(all_data_frames)
 
-  {yars = c(22, 23)
-    for (i in  month.abb) {
-      for (y in yars) {
-        if (file.exists(paste0('test/', i, ' ', y, '.xlsx'))) {
-          processed_name <- paste0(i, y)
-          
-          assign(processed_name, load_month(i, y))
-          
-        }
-      }
-    }
-  }
+# Factorize month and year columns
+tot_test$month = factor(tot_test$month, levels = month.abb)
+tot_test$year = factor(tot_test$year, levels = c(22, 23))
 
-  tot_test = bind_rows(
-    Jan22 %>% mutate(month = 'Jan', year = 22),
-    Feb22 %>% mutate(month = 'Feb', year = 22),
-    Mar22 %>% mutate(month = 'Mar', year = 22),
-    Apr22 %>% mutate(month = 'Apr', year = 22),
-    May22 %>% mutate(month = 'May', year = 22),
-    Jun22 %>% mutate(month = 'Jun', year = 22),
-    Jul22 %>% mutate(month = 'Jul', year = 22),
-    Aug22 %>% mutate(month = 'Aug', year = 22),
-    Sep22 %>% mutate(month = 'Sep', year = 22),
-    Oct22 %>% mutate(month = 'Oct', year = 22),
-    Nov22 %>% mutate(month = 'Nov', year = 22),
-    Dec22 %>% mutate(month = 'Dec', year = 22),
-    Jan23 %>% mutate(month = 'Jan', year = 23),
-    Feb23 %>% mutate(month = 'Feb', year = 23),
-    Mar23 %>% mutate(month = 'Mar', year = 23),
-    Apr23 %>% mutate(month = 'Apr', year = 23),
-    May23 %>% mutate(month = 'May', year = 23),
-    Jun23 %>% mutate(month = 'Jun', year = 23),
-    Jul23 %>% mutate(month = 'Jul', year = 23),
-    Aug23 %>% mutate(month = 'Aug', year = 23),
-    Sep23 %>% mutate(month = 'Sep', year = 23),
-    Oct23 %>% mutate(month = 'Oct', year = 23),
-    # un comment when Nov and dec exist
-    Nov23 %>% mutate(month = 'Nov', year = 23),
-    #Dec %>% mutate(month = 'Dec',year = 23)
-  )
-  tot_test$month = factor(tot_test$month, levels = month.abb)
-  tot_test$year = factor(tot_test$year, levels = c(22, 23))
   
   pgjoin = data.frame(products[, c(1:3)])
   
@@ -182,3 +160,6 @@ top_products_tomatoe = head(tot_test2 %>%
                                   arrange(desc(ProductValue)) %>%
                                    select(1),
                             3)
+
+
+
