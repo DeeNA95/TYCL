447dbc48d6f85864f1e9ceb89b5b4dc19e952a6b
Derek Amuna <dadadee02@outlook.com>
2023-12-22 03:50:20 +0000

    removed insights tab

--- body.R
+++ body.R
@@ -109,44 +109,7 @@ body = dashboardBody(
       box(title = textOutput('tptitlegraph'),plotlyOutput('topProductGraph',height = '450px'), width = '100%',solidHeader = T ,status = 'primary' )
     ),
 
-    ### Insights Tab
-    tabPanel(
-      'Insights',
-      value = 'INSIGHTS',
-
-      ## AVg sales per working day
-      box(
-        tableOutput('AvgSaPD'),
-        title = 'Daily Sales Average (Ghs)',
-        width = '100%',
-        collapsible = T,
-        solidHeader = T,
-        status = 'warning',
-        style = "font-family: 'Open Sans', sans-serif;"
-      ),
-
-      ## Highest Selling Product
-      box(
-        textOutput('HiSa',container = tags$h4,inline = T),
-
-        # Expected highest month
-        textOutput('ExMo',container = tags$h4,inline = T),
-        width = '100%',
-        collapsible = T,
-        solidHeader = T,
-        status = 'info',
-        style = "font-family: 'Open Sans', sans-serif;"
-      ),
-
-      ##Minimum stocks expected for the month
-      box(
-        title = textOutput('ExSt'),
-        dataTableOutput('ExStTable'),width = '100%',
-        collapsible = T,
-        solidHeader = T,
-        status = 'primary',style = "font-family: 'Open Sans', sans-serif;"
-      )
-    ),
+    
 
     ### Cross-section tab
     tabPanel(
